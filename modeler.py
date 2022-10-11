b2db50d34576c1fa5cd7f4d7e52ee1ae1c40e848
seung-beom-lee <sl84@illinois.edu>
2022-10-11 12:55:17 -0500

    Backup.

--- modeler.py
+++ modeler.py
@@ -8,7 +8,7 @@ class CNNModule(nn.Module):
     def __init__(self):
         super(CNNModule, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(in_channels = 10,
+            nn.Conv2d(in_channels = 100,
                       out_channels = 64,
                       kernel_size = 3,
                       stride = 1,
@@ -31,7 +31,7 @@ class CNNModule(nn.Module):
                          padding=1,
                          dilation=1),
             nn.ConvTranspose2d(in_channels = 64,
-                               out_channels = 1,
+                               out_channels = 100,
                                kernel_size = 4,
                                stride=2,
                                padding=1,
@@ -48,43 +48,36 @@ class MyClassifier(nn.Module):
         super(MyClassifier, self).__init__()
         self.models = []
         self.parameters = []
-        for ii  in range(10):
+        for i  in range(10):
             _model_ = CNNModule()
             self.models.append(_model_)
             self.parameters += list(_model_.parameters())
 
     def forward(self, x):
         outputs = []
-        for ii in range(len(self.models)):
-            outputs.append(self.models[ii](x))
+        for i in range(len(self.models)):
+            outputs.append(self.models[i](x))
         return torch.cat(outputs, 1)
     
 
 
-mymodel = MyClassifier()
-print(mymodel.models[0])
-_input = torch.randn((1,10,10,10))
-_target = torch.randint(0, 2, _input.shape, dtype=torch.float32)
-optimizer = optim.Adam(mymodel.parameters, lr=1.e-3)
-loss_func = nn.BCELoss()
-mymodel.train()
-for epoch in range(5):
-    optimizer.zero_grad()
-    _output = mymodel.forward(_input)
-    _loss = loss_func(_output, _target)
-    _loss.backward()
-    optimizer.step()
-    print(epoch, _loss.item())
+# mymodel = MyClassifier()
+# print(mymodel.models[0])
+# _input = torch.randn((1,10,10,10))
+# _target = torch.randint(0, 2, _input.shape, dtype=torch.float32)
+# optimizer = optim.Adam(mymodel.parameters, lr=1.e-3)
+# loss_func = nn.BCELoss()
+# mymodel.train()
+# for epoch in range(5):
+#     optimizer.zero_grad()
+#     _output = mymodel.forward(_input)
+#     _loss = loss_func(_output, _target)
+#     _loss.backward()
+#     optimizer.step()
+#     print(epoch, _loss.item())
 
-print(_input.shape)
-print(_target.shape)
+# print(_input.shape)
+# print(_target.shape)
 
-print(_output)
-print(_target)
-
-
-# plt.imshow(_output[0][0].detach().numpy())
-# plt.show()
-# plt.close()
-# plt.imshow(_target[0][0].detach().numpy())
-# plt.show()
+# print(_output)
+# print(_target)
